55d62e24780e2354117661719b0fd746fce6b1c8
Kian Naquines <kjgnaquines@gmail.com>
2024-08-29 00:25:18 +0800

    Update raspberrypi_install_script.sh

--- raspberrypi_install_script.sh
+++ raspberrypi_install_script.sh
@@ -1,6 +1,6 @@
 echo "Setting up necesary pakages"
 sudo apt upgdate -y && sudo apt upgrade -y
-sudo apt install -y raspberrypi-kernel-headers build-essential bc git
+sudo apt install -y raspberrypi-kernel-headers build-essential bc git libpcap-dev
 
 echo "Cloning the project"
 git clone https://github.com/lwfinger/rtw88
