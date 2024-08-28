1dd37ea25265c48b86d7e833af86a691d9ecdf86
Kian Naquines <kjgnaquines@gmail.com>
2024-08-28 23:32:48 +0800

    Update raspberrypi_install_script.sh
    added messages and monitor mode for testing

--- raspberrypi_install_script.sh
+++ raspberrypi_install_script.sh
@@ -1,6 +1,18 @@
+echo "Setting up necesary pakages"
 sudo apt upgdate -y && sudo apt upgrade -y
 sudo apt install -y raspberrypi-kernel-headers build-essential bc git
+
+echo "Cloning the project"
 git clone https://github.com/lwfinger/rtw88
 cd rtw88
+
+echo "Running make....."
 sudo make
+
+echo "Running make install"
 sudo make install
+
+echo "Setting up the wlan1 into monitor mode"
+sudo ip link set wlan1 down
+sudo iw dev wlan1 set type monitor
+sudo ip link set wlan1 up
